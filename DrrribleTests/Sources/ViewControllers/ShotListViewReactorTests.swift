7b37be6721749c0fc6c649f870aecc0d801040a4
Suyeol Jeon <devxoul@gmail.com>
2017-07-24 02:01:10 +0900

    Fix filename

--- DrrribleTests/Sources/ViewControllers/ShotListViewReactorTests.swift
+++ DrrribleTests/Sources/ViewControllers/ShotListViewReactorTests.swift
@@ -14,31 +14,67 @@ import RxSwift
 import RxTest
 
 @testable import Drrrible
-/*
+
 final class ShotListViewReactorTests: TestCase {
-  func testSections() {
-    RxExpect { test in
-      DI.register(ShotServiceType.self) { _ in
-        StubShotService().then {
-          $0.shotsClosure = { _ in .just(List(items: [ShotFixture.shot1, ShotFixture.shot2])) }
+  func testInitialState() {
+    let reactor = ShotListViewReactor(
+      shotService: StubShotService(),
+      shotCellReactorFactory: ShotCellReactor.init
+    )
+    XCTAssertEqual(reactor.currentState.isRefreshing, false)
+    XCTAssertEqual(reactor.currentState.isLoading, false)
+    XCTAssertEqual(reactor.currentState.nextURL, nil)
+    XCTAssertEqual(reactor.currentState.sections[0].items.count, 0)
+  }
+
+  func testRefresh() {
+    let nextURL = URL(string: "https://example.com")!
+    let shotService = StubShotService().then {
+      $0.stub($0.shots) { _ in
+        let shots = [ShotFixture.shot1, ShotFixture.shot2]
+        return .just(List<Shot>(items: shots, nextURL: nextURL))
+      }
+    }
+    let reactor = ShotListViewReactor(
+      shotService: shotService,
+      shotCellReactorFactory: ShotCellReactor.init
+    )
+    _ = reactor.state
+    reactor.action.onNext(.refresh)
+    let executions = shotService.executions(shotService.shots)
+    XCTAssertEqual(executions.count, 1)
+    XCTAssertEqual(executions[0].arguments, .refresh)
+    XCTAssertEqual(reactor.currentState.sections[0].items.count, 2)
+    XCTAssertEqual(reactor.currentState.nextURL, nextURL)
+  }
+
+  func testLoadMore() {
+    let shotService = StubShotService().then {
+      $0.stub($0.shots) { paging in
+        switch paging {
+        case .refresh:
+          let shots = [ShotFixture.shot1]
+          let nextURL = URL(string: "https://example.com?page=1")!
+          return .just(List<Shot>(items: shots, nextURL: nextURL))
+
+        case .next:
+          let shots = [ShotFixture.shot2]
+          let nextURL = URL(string: "https://example.com?page=2")!
+          return .just(List<Shot>(items: shots, nextURL: nextURL))
         }
       }
-      let reactor = ShotListViewReactor()
-      test.input(reactor.action, [
-        next(100, .refresh),
-      ])
-
-      let sectionItemCount = reactor.state
-        .map { $0.sections }
-        .map { $0[0].items.count }
-        .distinctUntilChanged()
-      test.assert(sectionItemCount)
-        .filterNext()
-        .equal([
-          0, // initial
-          2, // after refreshing
-        ])
     }
+    let reactor = ShotListViewReactor(
+      shotService: shotService,
+      shotCellReactorFactory: ShotCellReactor.init
+    )
+    _ = reactor.state
+    reactor.action.onNext(.refresh) // to set next url
+    reactor.action.onNext(.loadMore)
+    let executions = shotService.executions(shotService.shots)
+    XCTAssertEqual(executions.count, 2)
+    XCTAssertEqual(executions[1].arguments, .next(URL(string: "https://example.com?page=1")!))
+    XCTAssertEqual(reactor.currentState.sections[0].items.count, 2)
+    XCTAssertEqual(reactor.currentState.nextURL, URL(string: "https://example.com?page=2")!)
   }
 }
-*/
