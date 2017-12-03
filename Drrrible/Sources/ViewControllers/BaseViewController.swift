f7cff526aaa2741404f9123ad7359f13eb9c44e5
Suyeol Jeon <devxoul@gmail.com>
2017-12-04 02:12:04 +0900

    Fix weird scroll view issue on iOS 11

--- Drrrible/Sources/ViewControllers/BaseViewController.swift
+++ Drrrible/Sources/ViewControllers/BaseViewController.swift
@@ -20,6 +20,12 @@ class BaseViewController: UIViewController {
 
   var automaticallyAdjustsLeftBarButtonItem = true
 
+  /// There is a bug when trying to go back to previous view controller in a navigation controller
+  /// on iOS 11, a scroll view in the previous screen scrolls weirdly. In order to get this fixed,
+  /// we have to set the scrollView's `contentInsetAdjustmentBehavior` property to `.never` on
+  /// `viewWillAppear()` and set back to the original value on `viewDidAppear()`.
+  private var scrollViewOriginalContentInsetAdjustmentBehaviorRawValue: Int?
+
 
   // MARK: Initializing
 
@@ -48,8 +54,29 @@ class BaseViewController: UIViewController {
     if self.automaticallyAdjustsLeftBarButtonItem {
       self.adjustLeftBarButtonItem()
     }
+
+    // fix iOS 11 scroll view bug
+    if #available(iOS 11, *) {
+      if let scrollView = self.view.subviews.first as? UIScrollView {
+        self.scrollViewOriginalContentInsetAdjustmentBehaviorRawValue =
+          scrollView.contentInsetAdjustmentBehavior.rawValue
+        scrollView.contentInsetAdjustmentBehavior = .never
+      }
+    }
   }
 
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    // fix iOS 11 scroll view bug
+    if #available(iOS 11, *) {
+      if let scrollView = self.view.subviews.first as? UIScrollView,
+        let rawValue = self.scrollViewOriginalContentInsetAdjustmentBehaviorRawValue,
+        let behavior = UIScrollViewContentInsetAdjustmentBehavior(rawValue: rawValue) {
+        scrollView.contentInsetAdjustmentBehavior = behavior
+      }
+    }
+  }
 
   // MARK: Layout Constraints
 
